02e64e536eab3be1d8cfbbd264e3e4bbb60ad565
Alexsander998 <osa585848@bk.ru>
2024-06-26 18:52:40 +0300

    iter-1 каркас

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func Shorter(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Зашифровать"))
+}
+
+func Fuller(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Расшировать"))
+}
+
+func main() {
+	http.HandleFunc("/", Shorter)
+	http.HandleFunc("/{id}", Fuller)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Ахтунг сервер прилег: %s\n", err)
+	}
+}
