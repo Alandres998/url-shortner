0dda7c5a3d1d0650a10770d594e121e00774546e
Alexsander998 <osa585848@bk.ru>
2024-07-01 10:17:33 +0300

    iter5 накинул тесты на сервер

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,21 +15,30 @@ type ServerConfig struct {
 	ShortURL      string
 }
 
-func init() {
+func InitConfig() {
+	if os.Getenv("RUN_MODE") == "test" {
+		return
+	}
+
+	parseFlags()
+	loadEnv()
+}
+
+func parseFlags() {
 	flag.StringVar(&Options.ServerAdress.MainURLServer, "a", ":8080", "basic main address")
 	flag.StringVar(&Options.ServerAdress.ShortURL, "b", "http://localhost:8080", "short response address")
+	flag.Parse()
+}
 
+func loadEnv() {
 	if envMainURLServer := os.Getenv("SERVER_ADDRESS"); envMainURLServer != "" {
 		Options.ServerAdress.MainURLServer = envMainURLServer
 	}
 	if envShortURL := os.Getenv("BASE_URL"); envShortURL != "" {
 		Options.ServerAdress.ShortURL = envShortURL
 	}
-
-	flag.Parse()
 }
 
 func GetAdressServer(Port string) string {
-	text := fmt.Sprintf("http://localhost%s", Port)
-	return text
+	return fmt.Sprintf("http://localhost%s", Port)
 }
