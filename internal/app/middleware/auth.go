eb0dbaeb5cf49b1224c2991ae496974e9dc982a0
Alexsander998 <osa585848@bk.ru>
2024-08-14 17:51:01 +0300

    iter14 Проверка на установку куки

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -1,13 +1,20 @@
 package middlewares
 
 import (
+	"log"
+
 	"github.com/Alandres998/url-shortner/internal/app/service/auth"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 // Mидлварка на установка куки
 func AuthMiddleware() gin.HandlerFunc {
-
+	logger, errLog := zap.NewProduction()
+	defer logger.Sync()
+	if errLog != nil {
+		log.Fatalf("Не смог иницировать логгер")
+	}
 	return func(c *gin.Context) {
 		cookie, err := c.Cookie(auth.CookieName)
 		if err != nil || !auth.ValidateCookie(cookie) {
@@ -15,9 +22,17 @@ func AuthMiddleware() gin.HandlerFunc {
 			auth.SetUserCookie(c, userID)
 			c.Set(auth.CookieName, userID)
 			c.SetCookie(auth.CookieName, userID, 3600, "/", "localhost", false, true)
+			logger.Info("Cookie Set",
+				zap.String("Name", auth.CookieName),
+				zap.String("Value", userID),
+			)
 		} else {
 			c.Set(auth.CookieName, cookie)
 			c.SetCookie(auth.CookieName, cookie, 3600, "/", "localhost", false, true)
+			logger.Info("Cookie Set",
+				zap.String("Name", auth.CookieName),
+				zap.String("Value", cookie),
+			)
 		}
 		auth.InfoCookie(c, "Действия после мидлваркеAUth")
 		c.Next()
