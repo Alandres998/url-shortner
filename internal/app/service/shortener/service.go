2ac6c08d01e41eb80fceef0ad81683f553f47bab
Alexsander998 <osa585848@bk.ru>
2024-08-15 16:19:06 +0300

    iter15 Правка метода с горутинами

--- internal/app/service/shortener/service.go
+++ internal/app/service/shortener/service.go
@@ -3,6 +3,7 @@ package shortener
 import (
 	"context"
 	"math/rand"
+	"sync"
 
 	"github.com/Alandres998/url-shortner/internal/app/db/storage"
 	"github.com/Alandres998/url-shortner/internal/app/service/logger"
@@ -18,54 +19,54 @@ func GenerateShortURL() string {
 }
 
 func DeleteShortURL(userID string, shortURLs []string) {
+	// go func() {
+	// 	err := storage.Store.DeleteUserURL(context.Background(), shortURLs, userID)
+	// 	if err != nil {
+	// 		logger.LogError("DeleteShortURL", err.Error())
+	// 	}
+	// }()
+	const batchSize = 10
+
+	urlChan := make(chan string, len(shortURLs))
+	var wg sync.WaitGroup
+
+	// Заполняем канал URL-ами
 	go func() {
-		err := storage.Store.DeleteUserURL(context.Background(), shortURLs, userID)
-		if err != nil {
-			logger.LogError("DeleteShortURL", err.Error())
+		for _, shortURL := range shortURLs {
+			urlChan <- shortURL
+		}
+		close(urlChan)
+	}()
+
+	// Обрабатываем URL-ы пакетами
+	go func() {
+		defer wg.Wait()
+		var buffer []string
+		for shortURL := range urlChan {
+			buffer = append(buffer, shortURL)
+			if len(buffer) >= batchSize {
+				wg.Add(1)
+				go func(urls []string) {
+					defer wg.Done()
+					err := storage.Store.DeleteUserURL(context.Background(), urls, userID)
+					if err != nil {
+						logger.LogError("DeleteShortURL", err.Error())
+					}
+				}(buffer)
+				buffer = nil
+			}
+		}
+
+		// Обрабатываем оставшиеся URL-ы
+		if len(buffer) > 0 {
+			wg.Add(1)
+			go func(urls []string) {
+				defer wg.Done()
+				err := storage.Store.DeleteUserURL(context.Background(), urls, userID)
+				if err != nil {
+					logger.LogError("DeleteShortURL", err.Error())
+				}
+			}(buffer)
 		}
 	}()
-	// const batchSize = 10
-
-	// urlChan := make(chan string, len(shortURLs))
-	// var wg sync.WaitGroup
-
-	// // Заполняем канал URL-ами
-	// go func() {
-	// 	for _, shortURL := range shortURLs {
-	// 		urlChan <- shortURL
-	// 	}
-	// 	close(urlChan)
-	// }()
-
-	// // Обрабатываем URL-ы пакетами
-	// go func() {
-	// 	defer wg.Wait()
-	// 	var buffer []string
-	// 	for shortURL := range urlChan {
-	// 		buffer = append(buffer, shortURL)
-	// 		if len(buffer) >= batchSize {
-	// 			wg.Add(1)
-	// 			go func(urls []string) {
-	// 				defer wg.Done()
-	// 				err := storage.Store.DeleteUserURL(context.Background(), urls, userID)
-	// 				if err != nil {
-	// 					logger.LogError("DeleteShortURL", err.Error())
-	// 				}
-	// 			}(buffer)
-	// 			buffer = nil
-	// 		}
-	// 	}
-
-	// 	// Обрабатываем оставшиеся URL-ы
-	// 	if len(buffer) > 0 {
-	// 		wg.Add(1)
-	// 		go func(urls []string) {
-	// 			defer wg.Done()
-	// 			err := storage.Store.DeleteUserURL(context.Background(), urls, userID)
-	// 			if err != nil {
-	// 				logger.LogError("DeleteShortURL", err.Error())
-	// 			}
-	// 		}(buffer)
-	// 	}
-	// }()
 }
